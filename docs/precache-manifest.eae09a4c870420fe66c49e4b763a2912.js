667d481bebb7cdaadebc96eb929ad3fa79eaf9eb
TianyouDai <ddai060@uottawa.ca>
2019-01-17 10:24:51 -0500

    Added docs folder

---
+++ docs/precache-manifest.eae09a4c870420fe66c49e4b763a2912.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e5439c03dea6ee2b6df",
+    "url": "/static/js/main.2e5439c0.chunk.js"
+  },
+  {
+    "revision": "304bfe3baa46694fe3ba",
+    "url": "/static/js/1.304bfe3b.chunk.js"
+  },
+  {
+    "revision": "2e5439c03dea6ee2b6df",
+    "url": "/static/css/main.c823139b.chunk.css"
+  },
+  {
+    "revision": "e3345a0aa47e9a93013cfbe5f26de2a3",
+    "url": "/index.html"
+  }
+];
