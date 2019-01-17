8c5a5a9f4cc2594499d3d8fa6411fbdf8565262d
TianyouDai <ddai060@uottawa.ca>
2019-01-17 10:56:54 -0500

    branch for hosting

---
+++ precache-manifest.2e8d618993d89f5b28372ba59a9aad26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e7f96ec73f3f9fc4d9c",
+    "url": "/ShopifyCodeChallenge-React/static/js/runtime~main.3e7f96ec.js"
+  },
+  {
+    "revision": "9ab4ff905b42f7ae530d",
+    "url": "/ShopifyCodeChallenge-React/static/js/main.9ab4ff90.chunk.js"
+  },
+  {
+    "revision": "304bfe3baa46694fe3ba",
+    "url": "/ShopifyCodeChallenge-React/static/js/1.304bfe3b.chunk.js"
+  },
+  {
+    "revision": "9ab4ff905b42f7ae530d",
+    "url": "/ShopifyCodeChallenge-React/static/css/main.47cb51a1.chunk.css"
+  },
+  {
+    "revision": "e486c529d6c3503d3d3e37897885a00b",
+    "url": "/ShopifyCodeChallenge-React/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eae09a4c870420fe66c49e4b763a2912.js"
+  "/ShopifyCodeChallenge-React/precache-manifest.2e8d618993d89f5b28372ba59a9aad26.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ShopifyCodeChallenge-React/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
